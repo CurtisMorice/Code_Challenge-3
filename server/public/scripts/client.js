4cbd11ac3e7fbb6f6b5aa06e405a40d8a9e2a232
Christopher Black <chris@primeacademy.io>
2017-10-30 13:17:34 -0500

    updated code comment.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,7 +2,7 @@ var app = angular.module('denebApp', []);
 
 /** ---- CONTROLLER HERE ---- **/
 app.controller('DenebController', function(){
-  // atares is an array of student names
+  // deneb is an array of student names
   console.log(deneb);
   // @TODO: Write your controller code in here
 });
