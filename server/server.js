849b7b7ac31d7c9f3574692b8c0e21800c003077
Christopher Black <chris@primeacademy.io>
2018-03-26 10:27:25 -0500

    ES6

--- server/server.js
+++ server/server.js
@@ -1,11 +1,10 @@
 /** --- DO NOT MODIFY --- **/
-var express = require('express');
-var app = express();
-var path = require('path');
-var port = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('server/public'));
 
-app.listen(port, function (req, res) {
-  console.log('Listening on port', port);
+app.listen(PORT, (req, res) => {
+  console.log('Listening on port', PORT);
 });
