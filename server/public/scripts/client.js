367992caa789a854493041b5321ab6e4c0ce8f11
Christopher Black <chris@primeacademy.io>
2018-03-26 10:41:09 -0500

    Added console logs.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,7 +1,9 @@
 var app = angular.module('StudentApp', []);
+console.log('client.js loaded');
 
 /** ---- CONTROLLER HERE ---- **/
 app.controller('StudentController', function(){
+  console.log('StudentController loaded');
   // studentList is a global variable array of student names
   // don't overthink this, you don't need a $http request
   // the array already exists right here
